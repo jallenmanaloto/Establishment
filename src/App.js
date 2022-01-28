9a9cbfc2f850aa5379e15dfb802c84825144428d
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-28 23:28:32 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img
+        style={{
+          position: "absolute",
+          top: "40%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+          height: "40vh",
+          width: "30vw",
+          zIndex: 1,
+        }}
+        src={LoginBG}
+        alt="background"
+      /> */}
+      <Form />
     </div>
   );
 }
